d5f26da7ae4f2fcc287b170503eb62b325140d20
Justice-Dono <jordan.tewnion@gmail.com>
2026-05-14 10:22:18 -0600

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx) {
-		return new Response("Hello World!");
-	},
-};
+  async fetch(request, env) {
+
+    if (request.method === "POST") {
+
+      const formData = await request.formData();
+      const file = formData.get("file");
+
+      if (!file) {
+        return new Response("No file uploaded", {
+          status: 400
+        });
+      }
+
+      await env.MY_BUCKET.put(
+        file.name,
+        file.stream()
+      );
+
+      return new Response("Upload successful");
+    }
+
+    return new Response("Worker running");
+  }
+}
